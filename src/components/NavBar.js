cb25ffdb3a80192e01b44fba6b84362e2c614412
MWBradshaw <mbradshaw@claflin.edu>
2023-04-13 16:11:08 -0400

    This commit will add the styled-component package to the repo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components/native'
+import Link from 'react-router-dom';
+
+const NavLink = styled.Link({
+    textDecoration: 'none'
+});
 
 const NavBar = () => {
     return (
         <div>
-            <Link to="">Home</Link>
-            <Link to="">Projects</Link>
-            <Link to="">Work Experience</Link>
-            <Link to="">About Me</Link>
-            <Link to="src/assets/Matthew Bradshaw - Resume (School).pdf" download>Resume</Link>
-            <Link to={{ pathname: "https://github.com/MWBradshaw"}}>Github</Link>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/projects">Projects</NavLink>
+            <NavLink to="/workexperience">Work Experience</NavLink>
+            <NavLink to="/aboutme">About Me</NavLink>
+            <NavLink to="/assets/Matthew Bradshaw - Resume (School).pdf" download>Resume</NavLink>
+            <NavLink to={{ pathname: "https://github.com/MWBradshaw"}}>Github</NavLink>
         </div>
     );
 }
