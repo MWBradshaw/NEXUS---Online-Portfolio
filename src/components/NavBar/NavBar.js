f72e62947b5a067f3234bcb8ab59f549bfd6453a
MWBradshaw <mbradshaw@claflin.edu>
2023-04-18 15:37:16 -0400

    Updated css module for NavBar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,13 +5,17 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="projects">Projects</Link>
-            <Link to="workexperience">Work Experience</Link>
-            <Link to="about">About Me</Link>
-            <Link to="src/assets/Matthew Bradshaw - Resume (School).pdf" target="_blank" download>Resume</Link>
-            <Link to={{ pathname:"https://github.com/MWBradshaw"}} target="_blank">Github</Link>
+        <div className={styles.navbody}>
+            <div>
+                <h2><Link to="/">NEXUS</Link></h2>
+            </div>
+            <div>
+                <Link to="projects">Projects</Link>
+                <Link to="workexperience">Work Experience</Link>
+                <Link to="about">About Me</Link>
+                <Link to="src/assets/Matthew Bradshaw - Resume (School).pdf" target="_blank" download>Resume</Link>
+                <Link to={{ pathname:"https://github.com/MWBradshaw"}} target="_blank">Github</Link>
+            </div>
         </div>
     );
 }
