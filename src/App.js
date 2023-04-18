1af8ea99e05132e158ea0e86ee9df00dd3bc9998
MWBradshaw <mbradshaw@claflin.edu>
2023-04-18 11:10:10 -0400

    This commit will add css modules for pages and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar.js";
-import Home from "./pages/Home.js";
-import AboutMe from "./pages/AboutMe.js";
-import Projects from "./pages/Projects.js";
-import WorkExperience from "./pages/WorkExperience.js";
+import NavBar from "./components/NavBar/NavBar.js";
+import Home from "./pages/Home/Home.js";
+import AboutMe from "./pages/AboutMe/AboutMe.js";
+import Projects from "./pages/Projects/Projects.js";
+import WorkExperience from "./pages/WorkExperience/WorkExperience.js";
+
 
 function App() {
+
+  useEffect(() => {
+    document.title = "NEXUS";
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
---
+++ src/pages/WorkExperience/WorkExperience.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+const WorkExperience = () => {
+    return (
+        <div>
+            <h2>Work Experience</h2>
+        </div>
+    );
+}
+
+export default WorkExperience;
