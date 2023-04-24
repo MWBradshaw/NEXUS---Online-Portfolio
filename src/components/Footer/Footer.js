629d2478f378c95125e933772c6057e7576ef08c
MWBradshaw <mbradshaw@claflin.edu>
2023-04-23 21:12:00 -0400

    This commit will remove obsolete html files in repo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
     return(
-        <div>
+        <div className={styles.footerBody}>
             <p>matthewbradshaw875@gmail.com</p>
             <p>&copy; 2023 Matthew Bradshaw</p>
         </div>
