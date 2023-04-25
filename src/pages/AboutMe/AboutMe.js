748195c24c192b8a77fc33178eca92280e88009e
MWBradshaw <mbradshaw@claflin.edu>
2023-04-24 21:43:54 -0400

    This commit will add information to the About Me page

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -4,6 +4,8 @@ const AboutMe = () => {
     return (
         <div>
             <p>About Me</p>
+            <p>Hi! My name is Matthew Bradshaw and I am an aspiring front-end developer. As a young child, I always had an interest in art and technology and I see front end development as a way to combine those two passions.</p>
+            <p>Outside of that, I enjoy playing the guitar, exercising and reading.</p>
         </div>
     );
 }
