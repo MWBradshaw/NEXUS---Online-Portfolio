d8a3f2f9f1d4d3fb8d7bac7698c22334afe1d55d
MWBradshaw <mbradshaw@claflin.edu>
2023-04-24 21:55:08 -0400

    This commit will add programming languages for projects under the Projects page

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -4,6 +4,32 @@ const Projects = () => {
     return (
         <div>
             <h2>Projects</h2>
+            <div>
+                <p>Blockchain Prototype</p>
+
+                <div>JavaScript</div>
+            </div>
+
+            <div>
+                <p>Pomodoro Application</p>
+                <p>TypeScript</p>
+                <p>HTML</p>
+                <p>CSS</p>
+
+            </div>
+
+            <div>
+                <p>NEXUS (Front-End)</p>
+                <p>JavaScript</p>
+                <p>HTML</p>
+                <p>CSS</p>
+
+            </div>
+
+            <div>
+                <p>AI Chatbot</p>
+                <p>Python</p>
+            </div>
         </div>
     );
 }
