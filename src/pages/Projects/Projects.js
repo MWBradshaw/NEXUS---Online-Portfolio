9830b2f5ca353c75948ff5298c92bb8fa9278f53
MWBradshaw <mbradshaw@claflin.edu>
2023-04-26 17:53:31 -0400

    This commit will fix the card css problem in Projects.js

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -4,71 +4,76 @@ const Projects = () => {
     return (
         <div>
             <h2>Projects</h2>
-            <div className={styles.card}>
-                <div className={styles.cardContent}>
-                    <div className={styles.cardFront}>
-                        <p className={styles.cardTitle}>Blockchain Prototype</p>
+            <div className={styles.cardDisplay}>
+                <div className={styles.card}>
+                    <div className={styles.cardContent}>
+                        <div className={styles.cardFront}>
+                            <p className={styles.cardTitle}>Blockchain Prototype</p>
 
-                        <div>
-                            <p>JavaScript</p>
+                            <div className={styles.cardBody}>
+                                <p>JavaScript</p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className={styles.cardBack}>
-                        
-                    </div>
-                </div>
-            </div>
-
-            <div className={styles.card}>
-                <div className={styles.cardContent}>
-                    <div className={styles.cardFront}>
-                        <p className={styles.cardTitle}>Pomodoro Application</p>
-
-
-                        <div>
-                            <p>TypeScript</p>
-                            <p>HTML</p>
-                            <p>CSS</p>
+                        <div className={styles.cardBack}>
+                            
                         </div>
-                    </div>
-
-                    <div className={styles.cardBack}>
-
-                    </div>
-                </div>
-            </div>
-
-            <div className={styles.card}>
-                <div className={styles.cardContent}>
-                    <div className={styles.cardFront}>
-                        <p className={styles.cardTitle}>NEXUS (Front-End)</p>
-
-                        <div>
-                            <p>JavaScript</p>
-                            <p>HTML</p>
-                            <p>CSS</p>
-                        </div>
-                    </div>
-
-                    <div className={styles.cardBack}>
-
                     </div>
                 </div>
 
-                
-            </div>
+                <div className={styles.card}>
+                    <div className={styles.cardContent}>
+                        <div className={styles.cardFront}>
+                            <p className={styles.cardTitle}>Pomodoro Application</p>
 
-            <div className={styles.card}>
-                <div className={styles.cardContent}>
 
-                    <div className={styles.cardFront}>
-                        <p className={styles.cardTitle}>AI Chatbot</p>
-                        <p>Python</p>
+                            <div className={styles.cardBody}>
+                                <p>TypeScript</p>
+                                <p>HTML</p>
+                                <p>CSS</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.cardBack}>
+
+                        </div>
                     </div>
+                </div>
+
+                <div className={styles.card}>
+                    <div className={styles.cardContent}>
+                        <div className={styles.cardFront}>
+                            <p className={styles.cardTitle}>NEXUS (Front-End)</p>
+
+                            <div className={styles.cardBody}>
+                                <p>JavaScript</p>
+                                <p>HTML</p>
+                                <p>CSS</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.cardBack}>
+
+                        </div>
+                    </div>
+
                     
-                    <div className={styles.cardBack}>
+                </div>
+
+                <div className={styles.card}>
+                    <div className={styles.cardContent}>
+
+                        <div className={styles.cardFront}>
+                            <p className={styles.cardTitle}>AI Chatbot</p>
+
+                            <div className={styles.cardBody}>
+                                <p>Python</p>
+                            </div>
+                        </div>
                         
+                        <div className={styles.cardBack}>
+                            
+                        </div>
                     </div>
                 </div>
             </div>
