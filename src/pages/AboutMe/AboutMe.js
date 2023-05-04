3ad27465e9ab2a8697103d6a71377920a388a498
MWBradshaw <mbradshaw@claflin.edu>
2023-05-04 19:18:36 -0400

    This commit will update the Aboue Me component

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -3,9 +3,11 @@ import styles from './AboutMe.module.css'
 const AboutMe = () => {
     return (
         <div>
-            <p>About Me</p>
-            <p>Hi! My name is Matthew Bradshaw and I am an aspiring front-end developer. As a young child, I always had an interest in art and technology and I see front end development as a way to combine those two passions.</p>
-            <p>Outside of that, I enjoy playing the guitar, exercising and reading.</p>
+            <h2>About Me</h2>
+            <div>
+                <p>Hi! My name is Matthew Bradshaw and I am an aspiring front-end developer. As a young child, I always had an interest in art and technology and I see front end development as a way to combine those two passions.</p>
+                <p>Outside of that, I enjoy playing the guitar, exercising and reading.</p>
+            </div>
         </div>
     );
 }
