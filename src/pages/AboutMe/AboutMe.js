9b8229328a065d93e9f78e771d92b6b997036ec5
MWBradshaw <mbradshaw@claflin.edu>
2023-05-05 20:54:20 -0400

    This commit will add the headshot picture to the repo

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -1,12 +1,19 @@
-import styles from './AboutMe.module.css'
-;
+import styles from './AboutMe.module.css';
+import Headshot from "../../assets/Headshot.JPG";
+
 const AboutMe = () => {
     return (
         <div>
             <h2>About Me</h2>
             <div>
-                <p>Hi! My name is Matthew Bradshaw and I am an aspiring front-end developer. As a young child, I always had an interest in art and technology and I see front end development as a way to combine those two passions.</p>
-                <p>Outside of that, I enjoy playing the guitar, exercising and reading.</p>
+                <div>
+                    <p>Hi! My name is Matthew Bradshaw and I am an aspiring front-end developer. As a young child, I always had an interest in art and technology and I see front end development as a way to combine those two passions.</p>
+                    <p>Outside of that, I enjoy playing the guitar, exercising and reading.</p>
+                </div>
+
+                <div>
+                    <img src={Headshot} alt="The author's headshot"></img>
+                </div>
             </div>
         </div>
     );
