7055a496192f1b1cb276f700a6d01ae71b649628
MWBradshaw <mbradshaw@claflin.edu>
2023-05-06 16:42:51 -0400

    This commit will update the styling in the About Me repo

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -4,9 +4,9 @@ import Headshot from "../../assets/Headshot.JPG";
 const AboutMe = () => {
     return (
         <div>
-            <h2>About Me</h2>
             <div className={styles.aboutMeBody}>
                 <div className={styles.aboutMeTextBody}>
+                    <h2>About Me</h2>
                     <p>Hi! My name is Matthew Bradshaw and I am an aspiring front-end developer. As a young child, I always had an interest in art and technology and I see front end development as a way to combine those two passions.</p>
                     <p>Outside of that, I enjoy playing the guitar, exercising and reading.</p>
                 </div>
