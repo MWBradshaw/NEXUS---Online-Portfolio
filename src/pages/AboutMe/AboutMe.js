d84a43c55fadb1f3e7b8f48afa9089443ebe2f92
MWBradshaw <mbradshaw@claflin.edu>
2023-05-05 21:04:23 -0400

    This commit will update the alignment of the About Me component

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -5,14 +5,14 @@ const AboutMe = () => {
     return (
         <div>
             <h2>About Me</h2>
-            <div>
-                <div>
+            <div className={styles.aboutMeBody}>
+                <div className={styles.aboutMeTextBody}>
                     <p>Hi! My name is Matthew Bradshaw and I am an aspiring front-end developer. As a young child, I always had an interest in art and technology and I see front end development as a way to combine those two passions.</p>
                     <p>Outside of that, I enjoy playing the guitar, exercising and reading.</p>
                 </div>
 
                 <div>
-                    <img src={Headshot} alt="The author's headshot"></img>
+                    <img className={styles.headshot} src={Headshot} alt="The author's headshot"></img>
                 </div>
             </div>
         </div>
