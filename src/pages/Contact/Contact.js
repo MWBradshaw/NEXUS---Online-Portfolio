0fe416c20a8647255906a3a7c83ca3941f1a2bf0
MWBradshaw <mbradshaw@claflin.edu>
2023-05-08 21:05:27 -0400

    This commit will add elements to the Contact component

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,5 +1,15 @@
 const Contact = () => {
-
+    return (
+        <div>
+            <h2>Contact Me</h2>
+            <form>
+                <label>Your Name:</label>
+                <input></input>
+                <label>Email Address</label>
+                <input></input>
+            </form>
+        </div>
+    )
 }
 
 export default Contact;
