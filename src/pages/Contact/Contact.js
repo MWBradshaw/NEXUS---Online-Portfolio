2cc2e48e63deb1498ea2f648803b999df3d8673f
MWBradshaw <mbradshaw@claflin.edu>
2023-05-11 20:50:46 -0400

    This commt will add divs to the Contact repo

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,12 +2,17 @@ const Contact = () => {
     return (
         <div>
             <h2>Contact Me</h2>
-            <form>
-                <label>Your Name:</label>
-                <input></input>
-                <label>Email Address</label>
-                <input></input>
-            </form>
+            <div>
+                <div>
+                    <form>
+                        <label>Your Name:</label>
+                        <input></input>
+                        <label>Email Address</label>
+                        <input></input>
+                        <input type="submit" value="Submit"></input>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
