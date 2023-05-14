f8118a3da10e93cb377900d31d991e475edd7692
MWBradshaw <mbradshaw@claflin.edu>
2023-05-13 22:39:05 -0400

    This commit will add the fontawesome dependency to the repo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,11 @@
 import styles from "./Footer.module.css";
-
+import { FontAwesomeIcon
+ } from "@fortawesome/react-fontawesome";
+ import { faLinkedIn } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
     return(
         <div className={styles.footerBody}>
+            <FontAwesomeIcon icon={faLinkedIn} />
             <p>matthewbradshaw875@gmail.com</p>
             <p>&copy; 2023 Matthew Bradshaw</p>
         </div>
