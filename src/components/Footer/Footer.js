fed42f8fd64504a4bbe855c6a2faa13d7ca5ba20
MWBradshaw <mbradshaw@claflin.edu>
2023-05-14 22:00:06 -0400

    This commit will remove the FontAwesome dependency

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,8 @@
 import styles from "./Footer.module.css";
-import { FontAwesomeIcon
- } from "@fortawesome/react-fontawesome";
- import { faLinkedIn } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
     return(
         <div className={styles.footerBody}>
-            <FontAwesomeIcon icon={faLinkedIn} />
             <p>matthewbradshaw875@gmail.com</p>
             <p>&copy; 2023 Matthew Bradshaw</p>
         </div>
