1ac19d716b8e8f8af230755e1363878cb0ef6d26
MWBradshaw <mbradshaw@claflin.edu>
2023-05-24 20:49:20 -0400

    This commit will update the technical skills section in the Work Experience component

--- src/pages/WorkExperience/WorkExperience.js
+++ src/pages/WorkExperience/WorkExperience.js
@@ -10,8 +10,76 @@ const WorkExperience = () => {
             </div>
 
             <div>
-                <div>
+                <div className="skillHeader">
                     <h3>Technical Skills</h3>
+                </div>
+
+                <div className="skillsMainBody">
+                    <div className="skillsBar">
+                        <div class="skillsInfo">
+                            <p>HTML</p>
+                            <p>%</p>
+                        </div>
+
+                        <div className="bar">
+                            <span class="html"></span>
+                        </div>
+                    </div>
+
+                    <div className="skillsBar">
+                        <div class="skillsInfo">
+                            <p>CSS3</p>
+                            <p>90%</p>
+                        </div>
+
+                        <div className="bar">
+                            <span class="css"></span>
+                        </div>
+                    </div>
+
+                    <div className="skillsBar">
+                        <div class="skillsInfo">
+                            <p>JavaScript</p>
+                            <p>%</p>
+                        </div>
+
+                        <div className="bar">
+                            <span class="javascript"></span>
+                        </div>
+                    </div>
+
+                    <div className="skillsBar">
+                        <div class="skillsInfo">
+                            <p>ReactJS</p>
+                            <p>%</p>
+                        </div>
+
+                        <div className="bar">
+                            <span class="react"></span>
+                        </div>
+                    </div>
+
+                    <div className="skillsBar">
+                        <div class="skillsInfo">
+                            <p>Bootstrap 5</p>
+                            <p>%</p>
+                        </div>
+
+                        <div className="bar">
+                            <span class="bootstrap"></span>
+                        </div>
+                    </div>
+
+                    <div className="skillsBar">
+                        <div class="skillsInfo">
+                            <p>Python</p>
+                            <p>%</p>
+                        </div>
+
+                        <div className="bar">
+                            <span class="python"></span>
+                        </div>
+                    </div>
                 </div>
             </div>
 
