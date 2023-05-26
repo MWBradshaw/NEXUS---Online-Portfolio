52b185ad92d3db2da56b32adb6bf5f20bacb3d44
MWBradshaw <mbradshaw@claflin.edu>
2023-05-26 17:46:37 -0400

    This commit will remove the Technical Skills section and css in WorkExperience.module.css

--- src/pages/WorkExperience/WorkExperience.js
+++ src/pages/WorkExperience/WorkExperience.js
@@ -13,75 +13,8 @@ const WorkExperience = () => {
                 <div className={styles.skillHeader}>
                     <h3>Technical Skills</h3>
                 </div>
-
-                <div className={styles.skillsMainBody}>
-                    <div className={styles.skillsBar}>
-                        <div class={styles.skillsInfo}>
-                            <p>HTML</p>
-                            <p>%</p>
-                        </div>
-
-                        <div className={styles.bar}>
-                            <span class={styles.html}></span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillsBar}>
-                        <div class={styles.skillsInfo}>
-                            <p>CSS3</p>
-                            <p>90%</p>
-                        </div>
-
-                        <div className={styles.bar}>
-                            <span class={styles.css}></span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillsBar}>
-                        <div class={styles.skillsInfo}>
-                            <p>JavaScript</p>
-                            <p>%</p>
-                        </div>
-
-                        <div className={styles.bar}>
-                            <span class={styles.javascript}></span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillsBar}>
-                        <div class={styles.skillsInfo}>
-                            <p>ReactJS</p>
-                            <p>%</p>
-                        </div>
-
-                        <div className={styles.bar}>
-                            <span class={styles.react}></span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillsBar}>
-                        <div class={styles.skillsInfo}>
-                            <p>Bootstrap 5</p>
-                            <p>%</p>
-                        </div>
-
-                        <div className={styles.bar}>
-                            <span class={styles.bootstrap}></span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillsBar}>
-                        <div class={styles.skillsInfo}>
-                            <p>Python</p>
-                            <p>%</p>
-                        </div>
-
-                        <div className={styles.bar}>
-                            <span class={styles.python}></span>
-                        </div>
-                    </div>
-                </div>
             </div>
+                
 
             <div>
                 <div>
