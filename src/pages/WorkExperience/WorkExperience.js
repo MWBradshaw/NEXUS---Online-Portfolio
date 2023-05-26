dab3cc46ddfcd699468f449b775905b3db71f851
MWBradshaw <mbradshaw@claflin.edu>
2023-05-26 19:08:31 -0400

    This commit will update the divs in WorkExperience.js

--- src/pages/WorkExperience/WorkExperience.js
+++ src/pages/WorkExperience/WorkExperience.js
@@ -3,24 +3,24 @@ import styles from './WorkExperience.module.css';
 const WorkExperience = () => {
     return (
         <div>
-            <div>
-                <div>
+            <section>
+                <div className="one">
                     <h3>Relevant Courses</h3>
                 </div>
-            </div>
+            </section>
 
-            <div>
+            <section className="two">
                 <div className={styles.skillHeader}>
                     <h3>Technical Skills</h3>
                 </div>
-            </div>
+            </section>
                 
 
-            <div>
+            <section className="three">
                 <div>
                     <h3>Work Experience</h3>
                 </div>
-            </div>
+            </section>
         </div>
     );
 }
