24762311e8cc2f2ee109acefd3a97e0eac6798f0
MWBradshaw <mbradshaw@claflin.edu>
2023-05-29 15:22:30 -0400

    This commit will remove the code for the Technical Skills section of WorkExperience.js

--- src/pages/WorkExperience/WorkExperience.js
+++ src/pages/WorkExperience/WorkExperience.js
@@ -18,69 +18,9 @@ const WorkExperience = () => {
                     <h2>Technical</h2>
                     <h2>Skills</h2>
                 </div>
+
                 <div className={styles.innerContainer}>
-                    <div className={styles.skillBox}>
-                        <span className={styles.skillTitle}>HTML5</span>
-                        <div className={styles.skillBar}>
-                            <span className={styles.skillPercentHTML}>
-                                <span className={styles.tooltip}>%</span> 
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillBox}>
-                        <span className={styles.skillTitle}>CSS3</span>
-                        <div className={styles.skillBar}>
-                            <span className={styles.skillPercentCSS}>
-                                <span className={styles.tooltip}>%</span> 
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillBox}>
-                        <span className={styles.skillTitle}>JavaScript</span>
-                        <div className={styles.skillBar}>
-                            <span className={styles.skillPercentJavaScript}>
-                                <span className={styles.tooltip}>%</span> 
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillBox}>
-                        <span className={styles.skillTitle}>Python</span>
-                        <div className={styles.skillBar}>
-                            <span className={styles.skillPercentPython}>
-                                <span className={styles.tooltip}>%</span> 
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillBox}>
-                        <span className={styles.skillTitle}>Java</span>
-                        <div className={styles.skillBar}>
-                            <span className={styles.skillPercentJava}>
-                                <span className={styles.tooltip}>%</span> 
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillBox}>
-                        <span className={styles.skillTitle}>ReactJS</span>
-                        <div className={styles.skillBar}>
-                            <span className={styles.skillPercentReactJS}>
-                                <span className={styles.tooltip}>%</span> 
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className={styles.skillBox}>
-                        <span className={styles.skillTitle}>Bootstrap 5</span>
-                        <div className={styles.skillBar}>
-                            <span className={styles.skillPercentBootstrap}>
-                                <span className={styles.tooltip}>%</span> 
-                            </span>
-                        </div>
-                    </div>
+                    
                 </div>
             </section>
                 
