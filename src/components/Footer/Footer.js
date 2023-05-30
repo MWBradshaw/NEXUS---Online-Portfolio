5731611fcbeba1d1d41ff49d98763d907f0b481e
MWBradshaw <mbradshaw@claflin.edu>
2023-05-30 12:49:50 -0400

    This commit will wrap the FontAwesome components in a elements in the Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,13 @@
 import styles from "./Footer.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return(
         <div className={styles.footerBody}>
-            <FontAwesomeIcon style={{ height: 30, width: 30 }} icon={faLinkedinIn}></FontAwesomeIcon>
-            <FontAwesomeIcon style={{ height: 30, width: 30, paddingLeft: 15, paddingRight: 15 }} icon={faInstagram}></FontAwesomeIcon>
-            <FontAwesomeIcon style={{ height: 30, width: 30 }}icon={faGithub}></FontAwesomeIcon>
+            <a className={styles.footerIcon} href="https://www.linkedin.com/in/matthew-bradshaw-2742271ab/" target="_blank" rel="noreferrer" ><FontAwesomeIcon style={{ height: 30, width: 30 }} icon={faLinkedinIn}></FontAwesomeIcon></a>
+            <a className={styles.footerIcon} href="https://www.instagram.com/101robbs/" target="_blank" rel="noreferrer" ><FontAwesomeIcon style={{ height: 30, width: 30, paddingLeft: 15, paddingRight: 15 }} icon={faInstagram}></FontAwesomeIcon></a>
+            <a className={styles.footerIcon} href="https://github.com/MWBradshaw" target="_blank" rel="noreferrer" ><FontAwesomeIcon style={{ height: 30, width: 30 }}icon={faGithub}></FontAwesomeIcon></a>
             <p>matthewbradshaw875@gmail.com</p>
             <p>&copy; 2023 Matthew Bradshaw</p>
         </div>
