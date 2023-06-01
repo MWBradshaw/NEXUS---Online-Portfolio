e4cf724f6107ef122eb4bda585c941c84fad9e39
MWBradshaw <73087304+MWBradshaw@users.noreply.github.com>
2023-05-31 20:30:49 -0700

    This commit will update the sizes of the icons and add padding to the NavBar

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,9 @@ import { faGithub, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-sv
 const Footer = () => {
     return(
         <div className={styles.footerBody}>
-            <a className={styles.footerIcon} href="https://www.linkedin.com/in/matthew-bradshaw-2742271ab/" target="_blank" rel="noreferrer" ><FontAwesomeIcon style={{ height: 30, width: 30 }} icon={faLinkedinIn}></FontAwesomeIcon></a>
-            <a className={styles.footerIcon} href="https://www.instagram.com/101robbs/" target="_blank" rel="noreferrer" ><FontAwesomeIcon style={{ height: 30, width: 30, paddingLeft: 15, paddingRight: 15 }} icon={faInstagram}></FontAwesomeIcon></a>
-            <a className={styles.footerIcon} href="https://github.com/MWBradshaw" target="_blank" rel="noreferrer" ><FontAwesomeIcon style={{ height: 30, width: 30 }}icon={faGithub}></FontAwesomeIcon></a>
+            <a className={styles.footerIcon} href="https://www.linkedin.com/in/matthew-bradshaw-2742271ab/" target="_blank" rel="noreferrer" ><FontAwesomeIcon style={{ height: 25, width: 25 }} icon={faLinkedinIn}></FontAwesomeIcon></a>
+            <a className={styles.footerIcon} href="https://www.instagram.com/101robbs/" target="_blank" rel="noreferrer" ><FontAwesomeIcon style={{ height: 25, width: 25, paddingLeft: 15, paddingRight: 15 }} icon={faInstagram}></FontAwesomeIcon></a>
+            <a className={styles.footerIcon} href="https://github.com/MWBradshaw" target="_blank" rel="noreferrer" ><FontAwesomeIcon style={{ height: 25, width: 25 }}icon={faGithub}></FontAwesomeIcon></a>
             <p>matthewbradshaw875@gmail.com</p>
             <p>&copy; 2023 Matthew Bradshaw</p>
         </div>
