50a7dd03864a3aafe5569e6f2341c4c6980dff84
MWBradshaw <73087304+MWBradshaw@users.noreply.github.com>
2023-06-04 18:46:11 -0700

    This commit will add boilerplate code to the WorkExperience.js

--- src/pages/WorkExperience/WorkExperience.js
+++ src/pages/WorkExperience/WorkExperience.js
@@ -1,9 +1,31 @@
 import styles from './WorkExperience.module.css';
+import Carousel from 'react-bootstrap/Carousel';
+import { useState } from 'react';
 
 const WorkExperience = () => {
+    // const [index, setIndex] = useState(0);
+
+    // const handleSelect = (selectedIndex) => {
+    //     setIndex(selectedIndex);
+    // };
+
     return (
         <div className={styles.container}>
             <section className={styles.one}>
+                {/* <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="src/assets/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg"
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        
+                    </Carousel.Item>
+                </Carousel> */}
+
                 <div className={styles.innerContainer1}>
                     <div className={styles.innerContainer1Body}>
                         <div className={styles.slideHeader1}>
@@ -15,6 +37,7 @@ const WorkExperience = () => {
                 </div>
             </section>
 
+            
             <section className={styles.two}>
                 <div className={styles.innerContainer2}>
                     
