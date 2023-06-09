d7ededb4484694ca402aad3166a0218a6d0a8fdb
MWBradshaw <73087304+MWBradshaw@users.noreply.github.com>
2023-06-09 07:37:15 -0700

    Will add a styling import to the repo

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,6 +1,8 @@
+import styles from "./Contact.module.css";
+
 const Contact = () => {
     return (
-        <div>
+        <div className={styles.contactBody}>
             <h2>Contact Me</h2>
             <div>
                 <div>
